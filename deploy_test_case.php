c15f257820991a2ff3965a6a4f2ae0cb7e966839
dixon_ <dick.olsson@nodeone.se>
2011-08-15 22:57:46 +0300

    Abstracted and extended the testing framework. Awesome.

--- deploy_test_case.php
+++ deploy_test_case.php
@@ -9,23 +9,20 @@ class DeployWebTestCase extends DrupalWebTestCase {
    * Set up all sites.
    */
   function setUp() {
+    // Set up our origin site.
     $this->setUpSite('deploy_origin', array('entity', 'ctools', 'features', 'views', 'views_ui', 'uuid', 'deploy', 'deploy_ui', 'deploy_aggregator_views', 'deploy_example'));
 
     // Switch back to original site to be able to set up a new site.
     $this->switchSite('deploy_origin', 'simpletest_original_default');
 
+    // Set up one endpoint site.
     $this->setUpSite('deploy_endpoint', array('entity', 'ctools', 'features', 'uuid', 'services', 'rest_server', 'uuid_services', 'uuid_services_example'));
 
     // Switch back to origin site where we want to start.
     $this->switchSite('deploy_endpoint', 'deploy_origin');
 
-    // We need to change the URL and set User Agent header in order to reach
-    // the endpoint.
-    $endpoint = deploy_endpoint_load('deploy_example_endpoint');
-    $endpoint->service_config['url'] = url('api', array('absolute' => TRUE));
-    $user_agent = drupal_generate_test_ua($this->sites['deploy_endpoint']->databasePrefix);
-    $endpoint->service_config['headers'] = array('User-Agent' => $user_agent);
-    ctools_export_crud_save('deploy_endpoints', $endpoint);
+    // Edit an endpoint object to point to our endpoint site.
+    $this->editEndpoint('deploy_example_endpoint', 'deploy_endpoint');
   }
 
   /**
@@ -36,17 +33,12 @@ class DeployWebTestCase extends DrupalWebTestCase {
   function tearDown() {
     // Tear down current site.
     parent::tearDown();
-
-    // Tear down the origin.
-    file_unmanaged_delete_recursive($this->originalFileDirectory . '/simpletest/' . substr($this->sites['deploy_origin']->databasePrefix, 10));
-    // Remove all tables and the connection.
-    Database::setActiveConnection('deploy_origin');
-    $schema = drupal_get_schema(NULL, TRUE);
-    foreach ($schema as $name => $table) {
-      db_drop_table($name);
-    }
-    Database::setActiveConnection('default');
-    Database::removeConnection('deploy_origin');
+    // We are making it easy for us (but a bit hacky) by using this method to
+    // clean out other environments that we've created.
+    simpletest_clean_database();
+    simpletest_clean_temporary_directories();
+    registry_rebuild();
+    cache_clear_all('simpletest', 'cache');
   }
 
   /**
@@ -57,6 +49,7 @@ class DeployWebTestCase extends DrupalWebTestCase {
 
     call_user_func_array(array($this, 'parent::setUp'), $modules);
     $this->saveState($key);
+    variable_set('deploy_site_hash', md5(time()));
 
     // Save original settings after first setUp(). We need to be able to restore
     // them after subsequent calls to setUp().
@@ -84,13 +77,27 @@ class DeployWebTestCase extends DrupalWebTestCase {
    * Switch to a specific site.
    */
   function switchSite($from, $to) {
+    // This is used to test the switch.
+    $old_site_hash = variable_get('deploy_site_hash', '');
+
+    // Switch database connection. This is where the magic happens.
     Database::renameConnection('default', $from);
     Database::renameConnection($to, 'default');
+
+    // Reset static caches, so sites doesn't share them.
+    drupal_static_reset();
+    // Since variables ($conf) lives in the global namespace, we need to
+    // reinitalize them to not make sites share variables.
+    cache_clear_all('*', 'cache_bootstrap');
+    $GLOBALS['conf'] = variable_initialize();
 
     // No need to restore anything if we are switching to the original site.
     if ($to != 'simpletest_original_default') {
       $this->restoreState($to);
-      drupal_static_reset();
+
+      // Test the switch.
+      $new_site_hash = variable_get('deploy_site_hash', '');
+      $this->assertNotEqual($old_site_hash, $new_site_hash, t('Switch to site %key was successful.', array('%key' => $to)));
     }
   }
 
@@ -115,6 +122,21 @@ class DeployWebTestCase extends DrupalWebTestCase {
   }
 
   /**
+   * Edit an endpoint to make it point to a specific site in this test
+   * environment.
+   *
+   * This is needed in order for deployments to be able to reach sites in this
+   * test environment.
+   */
+  function editEndpoint($endpoint_name, $site_key) {
+    $endpoint = deploy_endpoint_load($endpoint_name);
+    $endpoint->service_config['url'] = url('api', array('absolute' => TRUE));
+    $user_agent = drupal_generate_test_ua($this->sites[$site_key]->databasePrefix);
+    $endpoint->service_config['headers'] = array('User-Agent' => $user_agent);
+    ctools_export_crud_save('deploy_endpoints', $endpoint);
+  }
+
+  /**
    * Deploy the plan.
    *
    * @param string $name
@@ -127,5 +149,23 @@ class DeployWebTestCase extends DrupalWebTestCase {
     }
     $deployment_plan = deploy_plan_load($name);
     $deployment_plan->deploy();
+  }
+
+  /**
+   * Code taken from TaxonomyWebTestCase::createTerm() since we can't extend
+   * that test case. Some simplifications are made though.
+   *
+   * @see TaxonomyWebTestCase::createTerm()
+   */
+  function createTerm() {
+    $term = new stdClass();
+    $term->name = $this->randomName();
+    $term->description = $this->randomName();
+    // Use the first available text format.
+    $term->format = db_query_range('SELECT format FROM {filter_format}', 0, 1)->fetchField();
+    // For our test cases it's enough to rely on the standard tags vocabulary.
+    $term->vocabulary_machine_name = 'tags';
+    taxonomy_term_save($term);
+    return $term;
   }
 }
