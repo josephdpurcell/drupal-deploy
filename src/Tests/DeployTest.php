8890adb127f0a28709fca7f33be7f474a3853e13
Tim Millwood <tim@millwoodonline.co.uk>
2015-11-24 17:17:21 +0000

    Adding Drupal test

--- src/Tests/DeployTest.php
+++ src/Tests/DeployTest.php
@@ -35,11 +35,36 @@ class DeployTest extends KernelTestBase {
   }
 
   /**
-   * Should always return true.
+   * Test deploying from CouchDB to CouchDB.
    */
   public function testDeployCouchDB() {
     $source = $this->deploy->createSource('http://localhost:5984/source');
     $target = $this->deploy->createTarget('http://localhost:5984/target');
+
+    // Create the source and the target databases.
+    $source->createDatabase('source');
+    $target->createDatabase('target');
+
+    // Add three docs to the source db.
+    for ($i = 0; $i < 3; $i++) {
+      list($id, $rev) = $source->putDocument(
+          array("foo" => "bar" . var_export($i, true)),
+          'id' . var_export($i, true)
+      );
+    }
+
+    $result = $this->deploy->push($source, $target);
+
+    $this->assertTrue(!isset($result['error']), 'Successful migration.');
+  }
+
+  /**
+   * Test deploying from Drupal to Drupal.
+   */
+  public function testDeployDrupal() {
+    $source = $this->deploy->createSource('http://localhost:8080/relaxed/default');
+    $target = $this->deploy->createTarget('http://localhost:8081/relaxed/default');
+
     $result = $this->deploy->push($source, $target);
 
     $this->assertTrue(!isset($result['error']), 'Successful migration.');
